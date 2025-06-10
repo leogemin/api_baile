417841930071b2acb435b138efcba59c82859313
leogemin <leonardo.gemin.pereira@gmail.com>
2025-06-09 21:01:19 -0300

    logs to prod test

--- api/main.py
+++ api/main.py
@@ -17,6 +17,7 @@ def get_by_hash(hash: str):
             password = encode(keyFile.read().strip())
         cipher_suite = Fernet(password)
         decoded_hash = decode(cipher_suite.decrypt(hash.encode()))
+        print(decoded_hash)
     except (InvalidToken, ValueError, Exception):
         return JSONResponse(content={"erro": "QRCode inválido"})
 
@@ -26,6 +27,7 @@ def get_by_hash(hash: str):
 
     cursor.execute("SELECT * FROM convidados WHERE Convite = ?", (decoded_hash,))
     row = cursor.fetchone()
+    print(row)
 
     if not row:
         return JSONResponse(content={"erro": "Convite não encontrado"})
